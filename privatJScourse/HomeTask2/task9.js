72691bc7e882a78fd91d41c7c114798142a8f7d0
allakirjanova <97600952+allakirjanova@users.noreply.github.com>
2022-01-20 15:30:23 +0200

    Update task9.js

--- privatJScourse/HomeTask2/task9.js
+++ privatJScourse/HomeTask2/task9.js
@@ -16,17 +16,18 @@
 // console.log(solution([1,1,1,1,1,1,1,1], [1, 0])); // [1, 1, 1, 1, 1, 1, 2, 1]
 // console.log(solution([9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],[1]));
 // // // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-let i = 0;
-let c = 0;
 let arr = [];
 function solution(a,b){
-  if (Array.isArray(a,b)){
-        c = a + b;
-        arr.push(c)
-        console.log(arr)}
-
-        i++;
-        if (i < a.length || i < b.length) solution(a,b);
+  let summ = 0;
+  for(let i = 0, k = 0; i < a.length, k < b.length; i++, k++){
+    summ = a[i] + b[k];
+    if(summ >= 10){
+      summ = Number(summ.toString().slice(1));
+      // console.log(arr[arr.length-1] + 1);
+      arr[arr.length-1] =  arr[arr.length-1] + 1;        
+    }
+    arr.push(summ)
+  }
+  console.log(arr);    
   }
 solution([2, 4, 3], [5, 6, 4]); //[8, 0, 7]
